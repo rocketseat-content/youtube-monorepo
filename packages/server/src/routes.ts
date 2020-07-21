8beb19f24464097c5b330c3af6ebc82d914ade1e
Diego Fernandes <diego.schell.f@gmail.com>
2020-07-21 12:02:12 -0300

    New server route

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -6,4 +6,8 @@ routes.get('/', (request, response) => {
   return response.json({ message: 'Hello World' })
 })
 
+routes.post('/posts', (request, response) => {
+  return response.json({ message: 'New post' })
+})
+
 export default routes
