da9d6fa6645848ba0a2dfcd56a2300d35085eafc
Diego Fernandes <diego.schell.f@gmail.com>
2020-07-21 13:10:24 -0300

    Sharing code

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(routes)
 
 app.listen(3333, () => {
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import api from '@omnirepo/axios-config'
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    api.get('/').then(response => {
+      console.log(response)
+    })
+  }, [])
+
   return <h1>Hello World</h1>
 }
 
